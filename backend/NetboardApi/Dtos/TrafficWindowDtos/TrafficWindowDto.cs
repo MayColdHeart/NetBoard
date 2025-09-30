c74e829976fc5704b971fe856edf97cc743f9a12
Rafael <rafasilva9537@gmail.com>
2025-09-29 21:50:12 -0300

    Change `TrafficWindowDto` properties to use `init` instead of `set`

--- backend/NetboardApi/Dtos/TrafficWindowDtos/TrafficWindowDto.cs
+++ backend/NetboardApi/Dtos/TrafficWindowDtos/TrafficWindowDto.cs
@@ -2,11 +2,11 @@ namespace NetboardApi.Dtos.TrafficWindowDtos;
 
 public record TrafficWindowDto
 {
-    public int Id { get; set; }
-    public int DeviceIp { get; set; }
-    public int Protocol { get; set; }
-    public int TotalSizeKbps { get; set; }
-    public int UploadSizeKbps { get; set; }
-    public int DownloadSizeKbps { get; set; }
-    public DateTimeOffset CreatedAt { get; set; }
+    public int Id { get; init; }
+    public int DeviceIp { get; init; }
+    public int Protocol { get; init; }
+    public int TotalSizeKbps { get; init; }
+    public int UploadSizeKbps { get; init; }
+    public int DownloadSizeKbps { get; init; }
+    public DateTimeOffset CreatedAt { get; init; }
 }
