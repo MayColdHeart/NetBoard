b2052ed5a2b871e55f811896c6639b04e99f9e45
Rafael <rafasilva9537@gmail.com>
2025-09-30 11:58:20 -0300

    Install axios and use GET from api on frontend

--- design/src/App.jsx
+++ design/src/App.jsx
@@ -1,4 +1,7 @@
 import './GlobalStyles.css'
+import axios from 'axios';
+import { API_URL } from './consts/urls';
+import { useEffect } from 'react';
 
 /*Importando componentes*/
 import Aside from './components/Aside/Aside'
@@ -6,6 +9,20 @@ import DashBoard from './components/DashBoard/DashBoard'
 import Control from './components/Control/Control'
 
 function App() {
+  useEffect(() => {
+    async function fetchTraffic() {
+      const options = { method: 'GET', url: `${API_URL}/network/traffic` };
+      try {
+        console.log("Start request...")
+        const { data } = await axios.request(options);
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchTraffic();
+  }, []);
 
   return (
 
